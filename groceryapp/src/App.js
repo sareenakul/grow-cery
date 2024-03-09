fc1d85d9522dcb2a0959cd2538e4b91e8c5d963e
Akul Sareen <sareen.akul@gmail.com>
2024-03-09 05:12:21 -0500

    Added the logo and searchBar

--- groceryapp/src/App.js
+++ groceryapp/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import React from "react";
+import {Route, Routes, BrowserRouter} from "react-router-dom";
+import Home from "./Pages/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="*" element={<div>
+          <h1>404 ERROR</h1>
+        </div>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ groceryapp/src/Pages/Home.js
@@ -0,0 +1,11 @@
+import NavBar from "../Components/NAVBAR/NavBar";
+
+const Home = () =>{
+    return(
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default Home;
